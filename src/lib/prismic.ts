ea164d58a0bc6fe675c798c25cb1028517c2d084
Sam Littlefair <sam.littlefair@prismic.io>
2022-05-03 18:31:50 +0100

    Retrieve preview ref manually

--- src/lib/prismic.ts
+++ src/lib/prismic.ts
@@ -1,6 +1,14 @@
 import { getRepositoryEndpoint, createClient } from '@prismicio/client'
 
-const endpoint = getRepositoryEndpoint('monogram')
-const prismicClient = () => createClient(endpoint)
+export const repositoryName = 'sam-onboarding-blog-5'
+const endpoint = getRepositoryEndpoint(repositoryName)
+
+const prismicClient = (prismicCookie) => {
+	// Access preview ref from preview cookie
+	const ref = prismicCookie?.[`${repositoryName}.prismic.io`]?.preview
+
+	// Pass preview ref to preview cookie
+	return createClient(endpoint, { ref })
+}
 
 export default prismicClient
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,12 @@
+<script>
+	import { repositoryName } from '$lib/prismic'
+</script>
+
 <svelte:head>
 	<script
 		async
 		defer
-		src="https://static.cdn.prismic.io/prismic.js?new=true&repo=monogram"></script>
+		src={`https://static.cdn.prismic.io/prismic.js?new=true&repo=${repositoryName}`}></script>
 </svelte:head>
 
 <slot />
